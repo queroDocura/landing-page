49a6fbbebd2c39a30621d50b8921f3efc7afbdc0
John Vidal <ba.john.vidal@gmail.com>
2020-08-06 17:53:54 -0300

    ts basic

---
+++ src/components/navbar-landing-page/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <a href="">Marketplace</a>
+        <a href="">Calculadora de doces</a>
+        <a href="">Blog</a>
+        <a href="">Banco de dados</a>
+        <a href="">Quem somos</a>
+        <a href="">Contato</a>
+      </nav>
+    </div>
+  );
+}
