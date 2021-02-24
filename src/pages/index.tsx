75e82499286f65e9ffa2b2757b75472a8459fd58
John Vidal <ba.john.vidal@gmail.com>
2021-02-24 01:26:13 -0300

    v0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <h1>Server side rendering Landing Page</h1>;
+  return <h1>Quero Doçuras - Server side rendering - v0.1</h1>;
 };
 
 export default Home;
