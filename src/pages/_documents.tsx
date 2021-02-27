a9bf11cf24b34ff4b11182aff2ac4939aa233891
John Vidal <ba.john.vidal@gmail.com>
2021-02-26 21:49:03 -0300

    _document added

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,20 @@
+import React, {ReactElement} from 'react';
+import Document, {Html, Head, Main, NextScript} from 'next/document';
+
+class MyDocument extends Document {
+  render(): ReactElement {
+    return (
+      <Html lang="pt">
+        <Head>
+          <meta httpEquiv="Content-Language" content="pt-br" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
